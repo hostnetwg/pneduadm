fff445ddde088de8242cb78176bb0863e64b648d
hostnetwg <hostnet.wg@gmail.com>
2024-12-30 00:05:27 +0100

    2024-12-30 00:05

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900">
                     <div class="mb-4">
                         <a href="{{ route('products.create') }}" class="inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                            Dodaj produkt ABC
+                            Dodaj produkt
                         </a>                      
                     </div>
                     <table class="table-auto w-full border-collapse border border-gray-200">
